24f62ad6c3cd44f2e49b3c5a28b8993c081822c7
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-07-07 00:02:13 +0300

    Task for spring-core some questions

---
+++ src/main/java/com/swampus/exercise/task/Task8TheoryQuestions.java
@@ -0,0 +1,25 @@
+package com.swampus.exercise.task;
+
+public class Task8TheoryQuestions {
+    private final String question1
+            = "What is difference between IOC,DI and Spring";
+    private final String question2
+            = "What is difference between @Autowire and @Inject";
+    private final String question3
+            = "What class in spring-core is responsible for Dependency Injections";
+    private final String question4
+            = "What is difference between Annotated and XML driven Spring ?";
+    private final String question5
+            = "What is Application Context ? What is beans.xml ? " +
+            " Where information about beans loaded in context is stored in XML`less Spring ?";
+    private final String question6
+            = "What is Google Guice ? What is Dagger ?";
+    private final String question7
+            = "XML`less Spring. How I can make a instance of a String and put it into context ?";
+    private final String question8
+            = "What annotation Qualifier Do ?";
+    private final String question9
+            = "What scopes do Class have in Spring Context ? What is Singleton Pattern";
+    private final String question10
+            = "What is Spring AOP ? What is difference in Injection by fields and in Constructor ?";
+}
